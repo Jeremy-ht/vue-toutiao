4b03acdc8a2af141331b81e7433ada29c11afda9
Jeremy <1198787527@qq.com>
2021-07-04 23:28:24 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 引入全局样式
+import './styles/index.less'
+
+// 引入 vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+
+// 动态设置 REM 基准值
+import 'amfe-flexible'
+
+// 全局注册vant
+Vue.use(Vant)
+
 Vue.config.productionTip = false
 
 new Vue({
