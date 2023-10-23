5e955aa09bf289f058d7e7e741c026f6fea2b5a2
黄涛 <Szhtjykj666>
2023-10-23 09:53:47 +0800

    修改video模块增加识别度

--- src/views/video/Index.vue
+++ src/views/video/Index.vue
@@ -1,19 +1,43 @@
 <template>
-  <div>
-    Video
+  <div class="app-container">
+    <div class="my-head">
+    </div>
+    <div class="my-body" ref="myBody">
+      <div class="my-page">
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'VideoIndex',
-  data () {
+  name: 'aidDetail',
+  props: {},
+  data() {
     return {
+      open: false,
+      loading: false,
+      form: {},
+      myBodyHeight: null,
+      applyList: [],
+      userData: {},
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        mutualAidId: this.$route.query.id,
+        checkStatus: '0'
+      },
+      mutualAidApplyList: [],
+      total: 0,
+      numGather: {
+        waitCheck: 0,
+        successCheck: 0,
+        rejectCheck: 0
+      },
+      title: ''
     }
+  },
+  methods: {
   }
 }
 </script>
-
-<style scoped>
-
-</style>
